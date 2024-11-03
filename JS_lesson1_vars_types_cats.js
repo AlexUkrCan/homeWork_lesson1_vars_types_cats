177c0a14852d2c60d748d0248e0f4d78a8895454
Oleksandr Drozdov <drozdov.sasha2017@gmail.com>
2024-11-03 14:58:58 -0800

    Перше домашнє завдання по javascript

---
+++ JS_lesson1_vars_types_cats.js
@@ -0,0 +1,46 @@
+let t= `hello`;
+console.log(t);
+let s = `owu`;
+console.log(s);
+let l = `com`;
+console.log(l);
+let d = `ua`;
+console.log(d);
+let first = 1;
+console.log(first);
+let second = 10;
+console.log(second);
+let minus = -999;
+console.log(minus);
+let dou = 3.14;
+console.log(dou);
+let flo = 2.7;
+console.log(flo);
+let v = 16;
+console.log(v);
+let pravda = true;
+console.log(pravda);
+let lie = false;
+console.log(lie);
+
+
+let firstName =`Олександр`;
+let middleName  =`Сергійович`;
+let lastName =`Дроздов`;
+let person=firstName + " " + middleName + " " + lastName;
+console.log(person);
+
+
+let a= 100;
+console.log(typeof a);
+let b = `100`;
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+let myName = prompt('Введіть cвоє імя', '');
+console.log(myName);
+let midName = prompt('Введіть як Вас По-Батькові', '');
+console.log(midName);
+let age = prompt('Введіть Ваш вік', '');
+console.log(age);
