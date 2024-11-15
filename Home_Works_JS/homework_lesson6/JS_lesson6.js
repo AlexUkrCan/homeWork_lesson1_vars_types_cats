5e1d3d9fa29a665fc34203ac3aac1c2ada2bb5d2
Oleksandr Drozdov <drozdov.sasha2017@gmail.com>
2024-11-14 23:34:47 -0800

    Додано шосте домашнє завдання по javascript

--- Home_Works_JS/homework_lesson6/JS_lesson6.js
+++ Home_Works_JS/homework_lesson6/JS_lesson6.js
@@ -53,4 +53,190 @@ console.log(arrToString);
 // let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
+let nums = [11,21,3];
+function sortNums(array, direction) {
+    if(direction === 'ascending') {
+        const sortedArray = array.sort((a,b) => a - b);
+    }else if (direction === 'descending'){
+        const sortedArray = array.sort((a,b) => b - a);
+    }
+    return array;
+}
+console.log(sortNums(nums, 'ascending'));
 
+//- є масив
+//  -- відсортувати його за спаданням за monthDuration
+//  -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//  -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+console.log(coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration));;
+
+console.log(coursesAndDurationArray.filter(value => value.monthDuration > 5));
+
+let newArrayCourses = coursesAndDurationArray.map(function (value, index) {
+    let newCoursesAndDuration = {
+        id: index + 1,
+        title: value.title,
+        monthDuration: value.monthDuration,
+    }
+    return newCoursesAndDuration;
+})
+console.log(newArrayCourses);
+
+//описати колоду карт (від 6 до туза без джокерів)
+//  - знайти піковий туз
+//  - всі шістки
+//  - всі червоні карти
+//  - всі буби
+//  - всі трефи від 9 та більше
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//     value: '', // '6'-'10', 'ace','jack','queen','king'
+//     color:'', // 'red','black'
+// }
+let suits = ['spade', 'diamond','heart', 'clubs'];
+let valueOfCards = ['6', '7', '8', '9', '10', 'jack','queen','king','ace']
+const deckOfCards = [];
+for (const suit of suits) {
+    for (const value of valueOfCards) {
+        const card = {cardSuit:suit, value:value};
+        if(suit === 'heart' || suit === 'diamond') {
+            card.color = 'red';
+        }else {
+            card.color = 'black';
+        }
+        deckOfCards.push(card);
+    }
+}
+console.log(deckOfCards);
+
+let findSpadeAce = deckOfCards.filter(value => value.value === 'ace' && value.cardSuit === 'spade');
+console.log(findSpadeAce);
+
+let findAllSix = deckOfCards.filter(value => value.value === '6');
+console.log(findAllSix);
+
+let findAllRedCards = deckOfCards.filter(value => value.color === 'red');
+console.log(findAllRedCards);
+
+let findAllDiamond = deckOfCards.filter(value => value.cardSuit === 'diamond');
+console.log(findAllDiamond);
+
+let findClubs = deckOfCards.filter(value =>value.cardSuit === 'clubs' && (value.value === '9' || value.value === '10' || value.value === 'jack' || value.value === 'queen' || value.value === 'king' || value.value === 'ace'));
+console.log(findClubs);
+
+//Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//     diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
+
+console.log(deckOfCards)
+
+let reduceArrays = deckOfCards.reduce((previousValue, card) => {
+    if(card.cardSuit === 'clubs') {
+        previousValue.clubs.push(card);
+    }else if(card.cardSuit === 'spade') {
+        previousValue.spades.push(card);
+    }else if(card.cardSuit === 'diamond') {
+        previousValue.diamonds.push(card);
+    }else if(card.cardSuit === 'heart') {
+        previousValue.hearts.push(card);
+    }
+
+    return previousValue;
+},{spades:[], diamonds:[], hearts:[], clubs:[]})
+console.log(reduceArrays);
+
+//взяти з arrays.js масив coursesArray
+// --написати пошук всіх об'єктів, в яких в modules є sass
+// --написати пошук всіх об'єктів, в яких в modules є docker
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+]
+
+let saasFind = (coursesArray.filter(value => {
+   return value.modules.includes('sass');
+}))
+console.log(saasFind);
+
+let dockerFind = (coursesArray.filter(value => {
+    return value.modules.includes('docker');
+}))
+console.log(dockerFind);
